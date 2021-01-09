cf4ca0b275afc57f323f0361dc539fbae0a07db9
PaperbagLife <43522930+PaperbagLife@users.noreply.github.com>
2021-01-08 19:05:06 -0500

    player now can navigate automatically

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class playerMovement : MonoBehaviour
 {
@@ -9,19 +10,62 @@ public class playerMovement : MonoBehaviour
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = 4.0f;
+    public NavMeshAgent agent;
+    public GameObject tileManagerObject;
+    public Dictionary<Vector2Int, int> visitLog;
+    public Camera cam;
+    
+    Vector2Int destination;
+    Vector3 move;
     void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        tileManagerObject = GameObject.Find("tileManager");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+    	List<Vector2Int> unvisited = new List<Vector2Int>();
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //controller.Move(move * Time.deltaTime * playerSpeed);
         if (move != Vector3.zero)
         {
             gameObject.transform.forward = move;
+        }
+
+        if (Input.GetMouseButtonDown(0)) 
+        {
+        	Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        	RaycastHit hit;
+        	if (Physics.Raycast(ray, out hit))
+        	{
+        		agent.SetDestination(hit.point);
+        		Debug.Log("destination" + hit.point);
+        		Debug.Log("cur:" + gameObject.transform.position);
+        	}
+        }
+
+
+        if (Input.GetKeyDown("n"))
+        {
+        	visitLog = tileManagerObject.GetComponent<tileManager>().visitLog;
+        	foreach(var item in visitLog)
+			{
+				if (item.Value == 0) 
+				{
+					unvisited.Add(item.Key);
+				}
+
+			}
+			if (unvisited.Count == 0)
+			{
+				Debug.Log("No unvisited tiles");
+			}
+			destination = unvisited[Random.Range (0, unvisited.Count)];
+			Debug.Log("Destination:" + destination);
+			agent.SetDestination(new Vector3(destination[0]+0.5f, 2, destination[1]-0.5f));
         }
     }
 }
